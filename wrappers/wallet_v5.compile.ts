d330c7a30c1dc02f27397dd48f877b0935d64130
Trinketer22 <trinketer22@localhost>
2025-04-06 22:51:21 +0300

    Convert to TOLK

--- wrappers/wallet_v5.compile.ts
+++ wrappers/wallet_v5.compile.ts
@@ -1,6 +1,6 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: ['contracts/wallet_v5.fc']
+    lang: 'tolk',
+    entrypoint: 'contracts/wallet_v5.tolk'
 };
